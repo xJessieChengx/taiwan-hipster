034bc9f4c1070fcd0c09b10b89fdd8b2211eb288
Jessie Cheng <xjessiechengx@gmail.com>
2019-08-26 11:28:52 +0800

    push

--- engine/currencySearch.py
+++ engine/currencySearch.py
@@ -2,5 +2,6 @@ import twder
 
 def currencySearch(search):
 	dollarTuple = twder.now_all()[search]
-	reply = '{}\n{}的即期賣出價：{}'.format(dollarTuple[0],search,dollarTuple[4])
+	#reply = '{}\n{}的即期賣出價：{}'.format(dollarTuple[0],search,dollarTuple[4])
+	reply = '{}\n{}\n現金買入價：{}\n現金賣出價：{}\n即期買入價：{}\n即期賣出價：{}'.format(dollerTuple[0],search,dollerTuple[1],dollerTuple[2],dollerTuple[3],dollerTuple[4])
 	return reply
